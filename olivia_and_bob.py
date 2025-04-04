8dc2b16a8513d0ac4e85e2fd255a6fba602efbf7
awoiibob <awoiibobwilly@gmail.com>
2025-04-04 11:30:29 +0300

    First Successful Simple Test

--- olivia_and_bob.py
+++ olivia_and_bob.py
@@ -1,13 +1,19 @@
 import unittest
 
 
-class TestFibonacci(unittest.TestCase):
+def fibonacci(first, second, length):
+    if length == 0:
+        return [first]
+    sequence = [first]  # Start with the first value
+    sequence += fibonacci(second, first + second, length - 1)  # Recursively append
+    return sequence  # Return the list instead of printing
 
-    # Test for valid Fibonacci sequence generation
+
+class TestFibonacci(unittest.TestCase):
     def test_valid_fibonacci(self):
-        self.assertEqual(PrintFibonacci(0, 1, 7), [0, 1, 1, 2, 3, 5, 8])
-        self.assertEqual(PrintFibonacci(2, 3, 5), [2, 3, 5, 8, 13])
-        self.assertEqual(PrintFibonacci(5, 8, 6), [5, 8, 13, 21, 34, 55])
+        self.assertEqual(fibonacci(0, 1, 6), [0, 1, 1, 2, 3, 5, 8])  # 7 terms
+        self.assertEqual(fibonacci(2, 3, 4), [2, 3, 5, 8, 13])  # 5 terms
+        self.assertEqual(fibonacci(5, 8, 5), [5, 8, 13, 21, 34, 55])  # 6 terms
 
 
 if __name__ == '__main__':
